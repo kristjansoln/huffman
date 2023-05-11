cfcd55154ab273683c0409ee526456a52413631e
Kristjan <24980294+kristjansoln@users.noreply.github.com>
2023-05-12 01:01:05 +0200

    Move freq calc and node generation to own function

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 # Kristjan Šoln
 
 
-# string = 'BCAADDDCCACACAC'
-string = '00001111111111111111111112222222222299999999999999'
 
-
-# Creating tree nodes
+"""
+NodeTree: A class that defines an individual node in the node tree.
+Each node has a left and a right child.
+"""
 class NodeTree(object):
 
     def __init__(self, left=None, right=None):
@@ -16,18 +16,14 @@ class NodeTree(object):
     def children(self):
         return (self.left, self.right)
 
-    def nodes(self):
-        return (self.left, self.right)
-
     def __str__(self):
         return '%s_%s' % (self.left, self.right)
 
-
 """
-huffman_code_tree: A function that takes a node tree and creates
+getCodingTable: A function that takes a node tree and creates
 a coding table from it.
 """
-def huffman_code_tree(node, left=True, binString=''):
+def getCodingTable(node, left=True, binString=''):
     # If we are at the bottom node, return dictionary item
     if type(node) is str:
         return {node: binString}
@@ -36,47 +32,66 @@ def huffman_code_tree(node, left=True, binString=''):
     d = dict()
     # Add left and right node to d, recursively
     # Left node is the less probable one, so it gets a one
-    d.update(huffman_code_tree(l, True, binString + '1'))
+    d.update(getCodingTable(l, True, binString + '1'))
     # Right node is the more probable one, so it gets a zero
-    d.update(huffman_code_tree(r, False, binString + '0'))
+    d.update(getCodingTable(r, False, binString + '0'))
 
     return d
 
 
-# Calculating frequency
-freq = {}
-for c in string:
-    if c in freq:
-        freq[c] += 1
-    else:
-        freq[c] = 1
+"""
+getFrequency: Takes a string of characters and returns a list
+of tuples containing unique characters and number of their
+occurences. The list is sorted by this frequency.
+"""
+def getFrequency(input_string):
+    freq = {}
+    for c in input_string:
+        if c in freq:
+            freq[c] += 1
+        else:
+            freq[c] = 1
 
-# Sort based on the second element of each item - the frequency.
-# Lambda function is an inline anonymous function. An alternative would be
-# to define a function which takes an array and returns the second element of it.
-freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+    # Sort based on the second element of each item - the frequency.
+    # Lambda function is an inline anonymous function. An alternative would be
+    # to define a function which takes an array and returns the second element of it.
+    freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+    return freq
 
-nodes = freq
+"""
+getNodeTree: create a node tree from a sorted list of unique characters and their frequencies.
+"""
+def getNodeTree(freq_array):
+    node_tree = freq_array
+    while len(node_tree) > 1:
+        # Select current nodes
+        (key1, c1) = node_tree[-1]
+        (key2, c2) = node_tree[-2]
+        # Create a node from current nodes. Key1 has lower probability than key2,
+        # therefore key1 is the left one (gets a zero), and key2 is the right one (gets a one).
+        node = NodeTree(key1, key2)
+        # Replace current two nodes with an object. Add the previous frequencies and append it.
+        node_tree = node_tree[:-2]
+        node_tree.append((node, c1 + c2))
+        # Resort the nodes based on the frequency.
+        node_tree = sorted(node_tree, key=lambda x: x[1], reverse=True)
 
-# TODO: Create a function from this
-# Create a node tree
-while len(nodes) > 1:
-    # Select current nodes
-    (key1, c1) = nodes[-1]
-    (key2, c2) = nodes[-2]
-    # Create a node from current nodes. Key1 has lower probability than key2,
-    # therefore key1 is the left one (gets a zero), and key2 is the right one (gets a one).
-    node = NodeTree(key1, key2)
-    # Replace current two nodes with an object. Add the previous frequencies and append it.
-    nodes = nodes[:-2]
-    nodes.append((node, c1 + c2))
-    # Resort the nodes based on the frequency.
-    nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+    return node_tree
 
-# nodes[0][0] is the top node
-huffmanCode = huffman_code_tree(nodes[0][0])
 
+
+string1 = '00001111111111111111111112222222222299999999999999'
+string2 = '1111111111111111111111111222222222222222222223333333333333334444444444555555556666666777777888889999'
+
+freq_list = getFrequency(string2)
+
+tree = getNodeTree(freq_list)
+
+# tree[0][0] is the top node of the tree
+codingTable = getCodingTable(tree[0][0])
+
+# Print out the coding table
 print(' Char | Huffman code ')
 print('----------------------')
-for (char, frequency) in freq:
-    print(' %-4r |%12s' % (char, huffmanCode[char]))
+for (char, frequency) in freq_list:
+    print(' %-4r |%12s' % (char, codingTable[char]))
